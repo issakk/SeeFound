fb82bc7b125f27eeac3b62375f52410471af9286
GitHub Action <action@github.com>
2021-08-05 12:52:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/issakk/seefound/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/issakk/seefound/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.issakk.seefound.listeners
 
+import com.github.issakk.seefound.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/issakk/seefound/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/issakk/seefound/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.issakk.seefound.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.issakk.seefound.MyBundle
 
 class MyApplicationService {
 
